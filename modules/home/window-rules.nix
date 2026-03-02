eea8910dc5813a62ac59e79dd946e2b2fb6f24bc
Dax Harris <git@dax.gay>
2026-03-02 11:48:33 -0500

    wip window rule rendering

--- modules/home/window-rules.nix
+++ modules/home/window-rules.nix
@@ -37,6 +37,8 @@ let
             at-startup = mkIfNotNull matcher.at-startup;
         };
     };
+
+    mkChild = value: key: optional (!isNull value.${key}) {${key} = value;};
 in
 {
     options.wayland.windowManager.niri.settings.window-rules = mkOption {
@@ -46,12 +48,144 @@ in
                 {
                     match = mkOptDefault (types.listOf matcherType) [ ];
                     exclude = mkOptDefault (types.listOf matcherType) [ ];
+                    on-open = {
+                        default-column-width = mkNullOr sizeType;
+                        default-window-height = mkNullOr sizeType;
+                        open-on-output = mkNullOr types.str;
+                        open-on-workspace = mkNullOr types.str;
+                        open-maximized = mkNullOr types.bool;
+                        open-maximized-to-edges = mkNullOr types.bool;
+                        open-fullscreen = mkNullOr types.bool;
+                        open-floating = mkNullOr types.bool;
+                        open-focused = mkNUllOr types.bool;
+                    };
+                    dynamic = {
+                        draw-border-with-background = mkNullOr types.bool;
+                        opacity = mkNullOr types.float;
+                        block-out-from = mkNullOr (types.enum ["screencast" "screen-capture"]);
+                        variable-refresh-rate = mkNullOr types.bool;
+                        default-column-display = mkNullOr (types.enum ["normal" "tabbed"]);
+                        default-floating-position = mkNullOr (types.submodule ({...}: {
+                            options = {
+                                x = mkOption {type = numberType;};
+                                y = mkOption {type = numberType;};
+                                relative-to = mkNullOr (types.enum [
+                                    "top-left"
+                                    "top-right"
+                                    "bottom-left"
+                                    "bottom-right"
+                                    "top"
+                                    "left"
+                                    "bottom"
+                                    "right"
+                                ]);
+                            };
+                        }));
+                        scroll-factor = mkNullOr types.float;
+                        geometry-corner-radius = mkNullOr numberType;
+                        clip-to-geometry = mkNullOr types.bool;
+                        tiled-state = mkNullOr types.bool;
+                        baba-is-float = mkNullOr types.bool;
+                        min-width = mkNullOr numberType;
+                        max-width = mkNullOr numberType;
+                        min-height = mkNullOr numberType;
+                        max-height = mkNullOr numberType;
+                        focus-ring = {
+                            enable = mkNullOr types.bool;
+                            width = mkNullOr numberType;
+                            active-color = mkNullOr types.str;
+                            inactive-color = mkNullOr types.str;
+                            urgent-color = mkNullOr types.str;
+                            active-gradient = mkNullOr gradientType;
+                            inactive-gradient = mkNullOr gradientType;
+                            urgent-gradient = mkNullOr gradientType;
+                        };
+                        border = {
+                            enable = mkNullOr types.bool;
+                            width = mkNullOr numberType;
+                            active-color = mkNullOr types.str;
+                            inactive-color = mkNullOr types.str;
+                            urgent-color = mkNullOr types.str;
+                            active-gradient = mkNullOr gradientType;
+                            inactive-gradient = mkNullOr gradientType;
+                            urgent-gradient = mkNullOr gradientType;
+                        };
+                        shadow = {
+                            enable = mkNullOr types.bool;
+                            softness = mkNullOr numberType;
+                            spread = mkNullOr numberType;
+                            offset = mkNullOr (mkSub {
+                                x = mkOptDefault numberType 0;
+                                y = mkOptDefault numberType 0;
+                            });
+                            draw-behind-window = mkNullOr types.bool;
+                            color = mkNullOr types.str;
+                            inactive-color = mkNullOr types.str;
+                        };
+                        tab-indicator = {
+                            enable = mkNullOr types.bool;
+                            hide-when-single-tab = mkNullOr types.bool;
+                            place-within-column = mkNullOr types.bool;
+                            gap = mkNullOr numberType;
+                            width = mkNullOr numberType;
+                            length = mkNullOr types.float;
+                            position = mkNullEnum [
+                                "left"
+                                "right"
+                                "top"
+                                "bottom"
+                            ];
+                            gaps-between-tabs = mkNullOr numberType;
+                            corner-radius = mkNullOr numberType;
+                            active-color = mkNullOr types.str;
+                            inactive-color = mkNullOr types.str;
+                            urgent-color = mkNullOr types.str;
+                            active-gradient = mkNullOr gradientType;
+                            inactive-gradient = mkNullOr gradientType;
+                            urgent-gradient = mkNullOr gradientType;
+                        };
+                    };
                 }
             )
         );
         default = [ ];
     };
     config.wayland.windowManager.niri._raw_settings = {
-
+        window-rule = map (rule: {
+            _children = concatLists (with rule; [
+                map (matcher: renderMatcher "match" matcher) match
+                map (matcher: renderMatcher "exclude" matcher) exclude
+                mkChild on-open "default-column-width"
+                mkChild on-open "default-window-height"
+                mkChild on-open "open-on-output"
+                mkChild on-open "open-on-workspace"
+                mkChild on-open "open-maximized"
+                mkChild on-open "open-maximized-to-edges"
+                mkChild on-open "open-fullscreen"
+                mkChild on-open "open-floating"
+                mkChild on-open "open-focused"
+                mkChild dynamic "draw-border-with-background"
+                mkChild dynamic "opacity"
+                mkChild dynamic "block-out-from"
+                mkChild dynamic "variable-refresh-rate"
+                mkChild dynamic "default-column-display"
+                mkChild dynamic "scroll-factor"
+                mkChild dynamic "geometry-corner-radius"
+                mkChild dynamic "clip-to-geometry"
+                mkChild dynamic "tiled-state"
+                mkChild dynamic "baba-is-float"
+                mkChild dynamic "min-width"
+                mkChild dynamic "max-width"
+                mkChild dynamic "min-height"
+                mkChild dynamic "max-height"
+                optional (!isNull dynamic.default-floating-position {
+                    default-floating-position._props = {
+                        x = dynamic.default-floating-position.x;
+                        y = dynamic.default-floating-position.y;
+                        relative-to = mkIfNotNull dynamic.default-floating-position.relative-to;
+                    };
+                })
+            ]);
+        }) cfg;
     };
 }
